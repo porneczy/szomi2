c9caa9c909c7050acfa306fca42ac2f0a0ba6f6d
porneczy <porneczy@gmail.com>
2023-04-20 14:13:02 +0200

    awards

--- src/pages/Awards.tsx
+++ src/pages/Awards.tsx
@@ -1,5 +1,48 @@
+import "@pages/Pages.scss";
+import { FiFacebook, FiInstagram } from "react-icons/fi";
+import awardsImg from "../resources/imgs/Awards/team1.jpg";
+
 function Awards(props: {}) {
-    return <div>Awards</div>;
+    return (
+        <div className="page-container">
+            <div className="awards-container">
+                <div className="awards-info-container">
+                    <div className="left-side">
+                        <h1>Eredményeink</h1>
+                        <p>
+                            Egy kis rövid összefoglalója és egyben öröm
+                            felsorolása, azoknak a munkával és gyakorlással
+                            töltött napoknak, heteknek, hónapoknak, amik
+                            lehetővé tették, hogy ilyen szép számmal érjünk el
+                            helyezéseket különböző versenyeken!
+                        </p>
+                        <div className="social-links">
+                            <a
+                                target="_blank"
+                                href="https://www.facebook.com/Szomszed.Gyor"
+                                rel="noreferrer"
+                            >
+                                <FiFacebook />
+                            </a>
+                            <a
+                                target="_blank"
+                                href="https://www.instagram.com/szomszed_gyor"
+                                rel="noreferrer"
+                            >
+                                <FiInstagram />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="right-side">
+                        <div className="awards-img">
+                            <img src={awardsImg} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="awards-list-container"></div>
+            </div>
+        </div>
+    );
 }
 
 export default Awards;
